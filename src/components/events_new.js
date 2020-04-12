6e2a3cfd892896ba715c816621c3a11d2bb66753
TAKUJI NODA <takujinoda@TAKUJInoMacBook-Pro.local>
2020-04-13 01:30:35 +0900

    Matrial UI

---
+++ src/components/events_new.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { Field, reduxForm } from 'redux-form'
+import { Link } from 'react-router-dom'
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
+
+import { postEvents } from '../actions'
+
+class EventsNew extends Component {
+  constructor(props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+  renderField(field) {
+    const { input, label, type, meta: { touched, error }} = field
+
+    return (
+      <TextField
+        hintStyle={label}
+        floatingLabelText={label}
+        type={type}
+        errorText={touched && error}
+        {...input}
+        fullWidth={true}
+      />
+    )
+
+    //<div>
+    //  <input {...input} placeholder={label} type={type} />
+    //  {touched && error && <span>{error}</span>}
+    //</div>)
+  }
+
+  async onSubmit(values) {
+    await this.props.postEvents(values)
+    this.props.history.push('/')
+  }
+
+
+
+  render() {
+    const { handleSubmit, pristine, submitting, invalid } = this.props
+    const style = { margin: 12}
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+
+        <div><Field label='Title' name="title" type="text" component={this.renderField} /></div>
+        <div><Field label='Body' name="body" type="text" component={this.renderField} /></div>
+
+        <div>
+          <RaisedButton label="Submit" type="submit" style={style} disabled={pristine || submitting || invalid } />
+          <RaisedButton label="Cancel" style={style} containerElement={<Link to="/" />} />
+        </div>
+
+      </form>
+    )
+  }
+}
+
+const validate = values => {
+  const errors = {}
+
+  if (!values.title) errors.title = "Enter a title, please."
+  if (!values.body) errors.body = "Enter a body, please."
+
+  return errors
+}
+
+const mapDispatchToProps = ({ postEvents })
+
+export default connect(null, mapDispatchToProps)(
+  reduxForm({ validate, form: 'eventNewForm' })(EventsNew)
+)
