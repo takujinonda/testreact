6954c12e212ca6ca791afa222d292a6e54e0f5ea
TAKUJI NODA <takujinoda@TAKUJInoMacBook-Pro.local>
2020-04-12 22:21:20 +0900

    inplement events index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
 import './index.css';
-import App from './App';
+import reducer from './reducers'
+
+import EventsIndex from './components/event_index';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(reducer, applyMiddleware(thunk))
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <EventsIndex />
+  </Provider>,
   document.getElementById('root')
 );
 
